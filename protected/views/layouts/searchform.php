96088b8b5f754b561529ca6c1e06c5328729aafa
Dockdep <Dockdep@bigmir.net>
2014-04-18 22:58:18 +0300

    ghf

---
+++ protected/views/layouts/searchform.php
@@ -0,0 +1,4 @@
+<form class="form-wrapper" action="./" method="get">
+	<input id="search" type="text" name="s" id="s" value="<?php the_search_query(); ?>" placeholder="Я ищу..." />
+	<input id="submit" type="submit" value="найти" />
+</form>
